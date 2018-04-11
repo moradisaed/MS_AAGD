07133a3994e5ef93d51ffe3b1dfbd39b835997d7
Saed Moradi <moradisaed@users.noreply.github.com>
2018-04-12 03:30:16 +0430

    Update final_AAGD.m

--- final_AAGD.m
+++ final_AAGD.m
@@ -1,6 +1,9 @@
 function out = final_AAGD( img,lmax,lmin )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+% small infrared target deetction using multi-scale
+% Absolute Average Gray Difference (AAGD) algorithm
+
+% Example:
+%          out=final_AAGD(img,[19,19,19,19],[3,5,7,9]);
 out1=zeros(size(img,1),size(img,2),length(lmin));
 for i=1:length(lmin)
     N=lmax(i)^2;
@@ -9,7 +12,7 @@ for i=1:length(lmin)
     mu_i=localmean(img,ones(lmin(i)));
     mu_e=localmean(img,ones(lmax(i)));
     mu_r=((N*mu_e)-(N1*mu_i))/N2;
-    out(:,:,i)=(mu_i-mu_r).^2;
+    out1(:,:,i)=(mu_i-mu_r).^2;
 end
 out=max(out1,[],3);
 end
